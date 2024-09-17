b2ea84249fa30c27f8d47fdafdd6ec68b6ca20fe
ahasanzadeh <hasanzadeh.amin@gmail.com>
2024-09-17 13:28:50 -0700

    Slightly modified

--- 104_EventDrivenSimulation/CustomerClass.cpp
+++ 104_EventDrivenSimulation/CustomerClass.cpp
@@ -1,33 +1,25 @@
-#include "pch.h"
 #include <iostream>
+
 using namespace std;
+
 #include "CustomerClass.h"
 
-CustomerClass::CustomerClass() {
-}
+CustomerClass::CustomerClass() {}
 CustomerClass::CustomerClass(int ID, int arrival) {
-	customerID = ID;
-	arrivalTime = arrival;
+  customerID = ID;
+  arrivalTime = arrival;
 }
-int CustomerClass::getID() const{
-	return customerID;
-}
+int CustomerClass::getID() const { return customerID; }
 
-int CustomerClass::getTime() const{
-	return arrivalTime;
-}
+int CustomerClass::getTime() const { return arrivalTime; }
 
-void CustomerClass::setID(int ID) {
-	customerID = ID;
-}
+void CustomerClass::setID(int ID) { customerID = ID; }
 
-void CustomerClass::setArrivalTime(int arrival) {
-	arrivalTime = arrival;
-}
+void CustomerClass::setArrivalTime(int arrival) { arrivalTime = arrival; }
 
 // operator overloading cout
-ostream& operator<<(ostream &os, const CustomerClass &iObj) {
-	os << " Customer ID: " << iObj.getID() << \
-		" Arrival Time: " << iObj.getTime() << endl;
-	return os;
+ostream &operator<<(ostream &os, const CustomerClass &iObj) {
+  os << " Customer ID: " << iObj.getID() << " Arrival Time: " << iObj.getTime()
+     << endl;
+  return os;
 }
--- 104_EventDrivenSimulation/ValidInputCheck.cpp
+++ 104_EventDrivenSimulation/ValidInputCheck.cpp
@@ -1,19 +1,17 @@
-#include "pch.h"
 #include <iostream>
 using namespace std;
 #include "ValidInputCheck.h"
 
 void ValidInputCheck(double &parameter, int lowerBound) {
-    bool validInputSeed = false;
-    while (!validInputSeed){
-        cin >> parameter;
-        if (cin.fail() || (lowerBound > parameter)) {
-            cin.clear();
-            cin.ignore(200, '\n');
-            cout << "Try again - enter a valid value: ";
-        }
-        else {
-            validInputSeed = true;
-        }
+  bool validInputSeed = false;
+  while (!validInputSeed) {
+    cin >> parameter;
+    if (cin.fail() || (lowerBound > parameter)) {
+      cin.clear();
+      cin.ignore(200, '\n');
+      cout << "Try again - enter a valid value: ";
+    } else {
+      validInputSeed = true;
     }
+  }
 }
